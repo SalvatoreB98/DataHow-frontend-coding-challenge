bb08ecf603119ff6f40b9e70efa45521b4fb7fc2
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-07-25 16:50:01 +0200

    Add search layout and axios call

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,20 @@ window.addEventListener("load",()=>{
     const app = new Vue({
         el: '#app',
         data: {
-            variable: 'variable'
+            searchedCountry : '',
+            countries : []
         },
         methods: {
     
         },
         mounted() {
-    
+            axios.get('https://covid-api.com/api/regions',{
+                params: ''
+            }
+            ).then((resp)=>{
+                this.countries = resp.data.data
+                console.log(resp.data.data)
+            })
         }
     });
 })
