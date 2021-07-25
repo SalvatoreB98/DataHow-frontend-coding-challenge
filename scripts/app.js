6e552ee280fad405150a2e3f6e393a05d3d3ad87
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-07-25 17:55:51 +0200

    graph importation OK

--- scripts/app.js
+++ scripts/app.js
@@ -6,16 +6,43 @@ window.addEventListener("load",()=>{
             countries : []
         },
         methods: {
-    
+            chart(){
+                let myChart = document.getElementById('myChart').getContext('2d');
+                let covidChart = new Chart(myChart, {
+                    type:'line',
+                    data:{
+                        labels:['19/02','20/02'],
+                        datasets:[{
+                            label:'date',
+                            data:[
+                                1,2,3,4,5,6,4,7,
+                            ]
+                        }]
+                    },
+                    options:{}
+                })
+            }
         },
         mounted() {
-            axios.get('https://covid-api.com/api/regions',{
-                params: ''
+            axios.get('https://covid-api.com/api/regions?order=name',{
+                params: {
+                    
+                }
             }
             ).then((resp)=>{
                 this.countries = resp.data.data
                 console.log(resp.data.data)
-            })
+            });
+            axios.get('https://covid-api.com/api/regions?order=name',{
+                params: {
+                    
+                }
+            }
+            ).then((resp)=>{
+                this.countries = resp.data.data
+                console.log(resp.data.data)
+            });
+            this.chart();
         }
     });
 })
