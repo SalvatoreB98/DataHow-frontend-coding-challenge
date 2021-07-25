8e3361a479ab75f23e0fc5c3e36ec97f8491153b
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-07-25 19:18:51 +0200

    import moment js

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,82 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     const app = new Vue({
         el: '#app',
         data: {
-            searchedCountry : '',
-            countries : []
+            searchedCountry: 'ITA',
+            countries: [],
+            currentDate: new Date(),
+            startDate: '',
+            stopDate: '',
+            data: []
         },
         methods: {
-            chart(){
+            chart(dateArray, data) {
                 let myChart = document.getElementById('myChart').getContext('2d');
                 let covidChart = new Chart(myChart, {
-                    type:'line',
-                    data:{
-                        labels:['19/02','20/02'],
-                        datasets:[{
-                            label:'date',
-                            data:[
-                                1,2,3,4,5,6,4,7,
-                            ]
+                    type: 'line',
+                    data: {
+                        labels: dateArray,
+                        datasets: [{
+                            label: 'cases',
+                            data: data
                         }]
                     },
-                    options:{}
+                    options: {}
                 })
+            },
+            onChange() {
+                axios.get(`https://covid-api.com/api/reports?date=2020-04-16&iso=${this.searchedCountry}`, {
+                    params: {
+
+                    }
+                }
+                ).then((resp) => {
+                    console.log(resp.data.data)
+                });
+            },
+            getDatesArray(startDate, endDate) {
+                var now = moment(startDate,["YYYY-DD-MM"]), dates = [];
+                while (now.isSameOrBefore(endDate)) {
+                    dates.push(now.format("YYYY-DD-MM"));
+                    now.add(1, 'days');
+                }
+                return dates;
+
+
+
+            },
+            getCases(){
+                dates = this.getDatesArray();
+                dates.forEach(date => {
+                    axios.get(`https://covid-api.com/api/reports?date=${date}&iso=${this.searchedCountry}`).
+                    then((resp) => {
+                        this.data = resp.data.data
+                        console.log(resp.data.data)
+                    });
+                });
+               
             }
+
         },
         mounted() {
-            axios.get('https://covid-api.com/api/regions?order=name',{
+            //get countries with axios
+            axios.get('https://covid-api.com/api/regions?order=name', {
                 params: {
-                    
+
                 }
             }
-            ).then((resp)=>{
+            ).then((resp) => {
                 this.countries = resp.data.data
-                console.log(resp.data.data)
+                // console.log(resp.data.data)
             });
-            axios.get('https://covid-api.com/api/regions?order=name',{
-                params: {
-                    
-                }
-            }
-            ).then((resp)=>{
-                this.countries = resp.data.data
-                console.log(resp.data.data)
-            });
-            this.chart();
+            currentDate = moment(this.currentDate)
+            var startDate = currentDate.subtract(90, "days").format("YYYY-MM-DD");
+            var startDate = moment('2021-01-02');
+            var endDate = moment('2021-01-12');
+            var dateList = this.getDatesArray(startDate,endDate);
+            console.log(dateList);
+            this.getCases();
+            this.chart(dateList,);
         }
     });
 })
